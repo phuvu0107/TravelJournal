afb4341b7ea25807e10b1a226967a93e8a5c776c
Truong Phu Vu <truongphu2842@gmail.com>
2022-03-23 00:50:42 -0700

    Icon

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ export default function Main(props){
             <img src={props.imageUrl} alt="main pic" className="card--image" />
             <div className="text--container">
                 <div className="card--location--container">
-                    <img src="/locationicon.png" alt="location icon" className="card--icon"/>
+                    <img src="assets/locationicon.png" alt="location icon" className="card--icon"/>
                     <p className="location-description">{props.location}</p>
                     <a href={props.googleMapsUrl} className="hyperlink" target="_blank">View on Google Maps</a>
                 </div>
